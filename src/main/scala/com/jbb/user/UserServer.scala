cb2cdd6a353ae90f8f50e7e188cf76525f557a58
Josh Buchanan <JoshuaBuchanan13@gmail.com>
2024-05-07 19:22:10 -0600

    Adding resource

--- src/main/scala/com/jbb/user/UserServer.scala
+++ src/main/scala/com/jbb/user/UserServer.scala
@@ -1,11 +1,10 @@
 package com.jbb.user
 
-import cats.effect.Async
+import cats.effect.{Async, Resource}
 import com.jbb.user.routes.UserServiceRoute
 import com.jbb.user.services.UserService
 import com.jbb.user.store.UserStore
 import com.jbb.user.util.db.DoobieDb
-import doobie.util.transactor.Transactor
 import fs2.io.net.Network
 import org.http4s.ember.server.EmberServerBuilder
 import org.http4s.server.middleware.Logger
@@ -16,30 +15,25 @@ object UserServer:
 
   def run[F[_]: Async: Network](appConfig: AppConfig): F[Nothing] = {
     // TODO: Mess with dependency injection to clean this up.
-    
+
     // given AppLogger[F] = Slf4jLogger.getLogger[F]
     val serverConfig = appConfig.server
     val db = appConfig.db
 
-    // db
-    val dbTransactor: Transactor[F] = DoobieDb.impl[F](
-      driver = db.driver,
-      url = db.url,
-      user = db.user,
-      password = db.password,
-    ).transactor
-
-    // stores
-    val userStore = UserStore.impl[F](dbTransactor)
-
-    // Services
-    val userService: UserService[F] = UserService.impl[F](userStore)
-
-    val httpApp = (UserServiceRoute.routes[F](userService)).orNotFound
-    val finalHttpApp = Logger.httpApp(true, true)(httpApp)
-
-
     for {
+      dbTransactor <- Resource.Pure(DoobieDb.impl[F](
+        driver = db.driver,
+        url = db.url,
+        user = db.user,
+        password = db.password,
+      ).transactor)
+      // Stores 
+      userStore = UserStore.impl[F](dbTransactor)
+      // Services 
+      userService: UserService[F] = UserService.impl[F](userStore)
+      // Routes 
+      httpApp = (UserServiceRoute.routes[F](userService)).orNotFound
+      finalHttpApp = Logger.httpApp(true, true)(httpApp)
       _ <-
         EmberServerBuilder.default[F]
           .withHost(serverConfig.host)
